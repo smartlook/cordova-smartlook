bf870b77617e24d18abc4d08982fdada850e1349
František Spurný <frantisekspurny@Frantisek-MacBook-Pro.local>
2019-07-16 16:57:44 +0200

    fix

--- www/SmartlookPlugin.js
+++ www/SmartlookPlugin.js
@@ -20,114 +20,100 @@ const SET_GLOBAL_EVENT_PROPERTY = "setGlobalEventProperty";
 const REMOVE_GLOBAL_EVENT_PROPERTY = "removeGlobalEventProperty";
 const REMOVE_ALL_GLOBAL_EVENT_PROPERTIES = "removeAllGlobalEventProperties";
 
-exports.setupAndStartRecording = function (params, success, error) {
+var emptyCallback = function() { return; };
+
+exports.setupAndStartRecording = function (params) {
     if (params["smartlookAPIKey"] != undefined) {
         if (params["fps"] != undefined) {
-            exec(success, error, SMARTLOOK_PLUGIN, SETUP_AND_START_RECORDING, [params["smartlookAPIKey"], params["fps"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SETUP_AND_START_RECORDING, [params["smartlookAPIKey"], params["fps"]]);
         } else {
-            exec(success, error, SMARTLOOK_PLUGIN, SETUP_AND_START_RECORDING, [params["smartlookAPIKey"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SETUP_AND_START_RECORDING, [params["smartlookAPIKey"]]);
         }
-    } else {
-        error("You need to provide smartlookAPIKey")
     }
 };
 
-exports.setup = function (params, success, error) {
+exports.setup = function (params) {
     if (params["smartlookAPIKey"] != undefined) {
         if (params["fps"] != undefined) {
-            exec(success, error, SMARTLOOK_PLUGIN, SETUP, [params["smartlookAPIKey"], params["fps"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SETUP, [params["smartlookAPIKey"], params["fps"]]);
         } else {
-            exec(success, error, SMARTLOOK_PLUGIN, SETUP, [params["smartlookAPIKey"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SETUP, [params["smartlookAPIKey"]]);
         }
-    } else {
-        error("You need to provide smartlookAPIKey")
     }
 };
 
-exports.startRecording = function (success, error) {
-    exec(success, error, SMARTLOOK_PLUGIN, START_RECORDING, []);
+exports.startRecording = function () {
+    exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_RECORDING, []);
 };
 
-exports.stopRecording = function (success, error) {
-    exec(success, error, SMARTLOOK_PLUGIN, STOP_RECORDING, []);
+exports.stopRecording = function () {
+    exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, STOP_RECORDING, []);
 };
 
-exports.isRecording = function (success, error) {
-    exec(success, error, SMARTLOOK_PLUGIN, IS_RECORING, []);
+exports.isRecording = function (result) {
+    exec(result, emptyCallback, SMARTLOOK_PLUGIN, IS_RECORING, []);
 };
 
-exports.startFullscreenSensitiveMode = function (success, error) {
-    exec(success, error, SMARTLOOK_PLUGIN, START_FULLSCREEN_SENSITIVE_MODE, []);
+exports.startFullscreenSensitiveMode = function () {
+    exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_FULLSCREEN_SENSITIVE_MODE, []);
 };
 
-exports.stopFullscreenSensitiveMode = function (success, error) {
-    exec(success, error, SMARTLOOK_PLUGIN, STOP_FULLSCREEN_SENSITIVE_MODE, []);
+exports.stopFullscreenSensitiveMode = function () {
+    exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, STOP_FULLSCREEN_SENSITIVE_MODE, []);
 };
 
-exports.isFullscreenSensitiveModeActive = function (success, error) {
-    exec(success, error, SMARTLOOK_PLUGIN, IS_FULLSCREEN_SENSITIVE_MODE_ACTIVE, []);
+exports.isFullscreenSensitiveModeActive = function (result) {
+    exec(result, emptyCallback, SMARTLOOK_PLUGIN, IS_FULLSCREEN_SENSITIVE_MODE_ACTIVE, []);
 };
 
-exports.setUserIdentifier = function (params, success, error) {
+exports.setUserIdentifier = function (params) {
     if (params["identifier"] != undefined) {
         if (params["sessionProperties"] != undefined) {
-            exec(success, error, SMARTLOOK_PLUGIN, SET_USER_IDENTIFIER, [params["identifier"], params["sessionProperties"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SET_USER_IDENTIFIER, [params["identifier"], params["sessionProperties"]]);
         } else {
-            exec(success, error, SMARTLOOK_PLUGIN, SET_USER_IDENTIFIER, [params["identifier"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SET_USER_IDENTIFIER, [params["identifier"]]);
         }
-    } else {
-        error("You need to provide identifier")
     }
 };
 
-exports.startTimedCustomEvent = function (params, success, error) {
+exports.startTimedCustomEvent = function (params) {
     if (params["name"] != undefined) {
         if (params["eventProperties"] != undefined) {
-            exec(success, error, SMARTLOOK_PLUGIN, START_TIMED_CUSTOM_EVENT, [params["name"], params["eventProperties"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_TIMED_CUSTOM_EVENT, [params["name"], params["eventProperties"]]);
         } else {
-            exec(success, error, SMARTLOOK_PLUGIN, START_TIMED_CUSTOM_EVENT, [params["name"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_TIMED_CUSTOM_EVENT, [params["name"]]);
         }
-    } else {
-        error("You need to provide event name")
     }
 };
 
-exports.trackCustomEvent = function (params, success, error) {
+exports.trackCustomEvent = function (params) {
     if (params["name"] != undefined) {
         if (params["eventProperties"] != undefined) {
-            exec(success, error, SMARTLOOK_PLUGIN, TRACK_CUSTOM_EVENT, [params["name"], params["eventProperties"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, TRACK_CUSTOM_EVENT, [params["name"], params["eventProperties"]]);
         } else {
-            exec(success, error, SMARTLOOK_PLUGIN, TRACK_CUSTOM_EVENT, [params["name"]]);
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, TRACK_CUSTOM_EVENT, [params["name"]]);
         }
-    } else {
-        error("You need to provide event name")
     }
 };
 
-exports.setGlobalEventProperties = function (params, success, error) {
+exports.setGlobalEventProperties = function (params) {
     if (params["globalEventProperties"] != undefined && params["immutable"] != undefined) {
-        exec(success, error, SMARTLOOK_PLUGIN, SET_GLOBAL_EVENT_PROPERTIES, [params["globalEventProperties"], params["immutable"]]);
-    } else {
-        error("You need to provide globalEventProperties and immutable flag")
+        exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SET_GLOBAL_EVENT_PROPERTIES, [params["globalEventProperties"], params["immutable"]]);
     }
 };
 
-exports.setGlobalEventProperty = function (params, success, error) {
+exports.setGlobalEventProperty = function (params) {
     if (params["key"] != undefined && params["value"] != undefined && params["immutable"] != undefined) {
-        exec(success, error, SMARTLOOK_PLUGIN, SET_GLOBAL_EVENT_PROPERTY, [params["key"], params["value"], params["immutable"]]);
-    } else {
-        error("You need to provide key, value and immutable flag")
+        exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SET_GLOBAL_EVENT_PROPERTY, [params["key"], params["value"], params["immutable"]]);
     }
 };
 
-exports.removeGlobalEventProperty = function (params, success, error) {
+exports.removeGlobalEventProperty = function (params) {
     if (params["key"] != undefined) {
-        exec(success, error, SMARTLOOK_PLUGIN, REMOVE_GLOBAL_EVENT_PROPERTY, [params["key"]]);
-    } else {
-        error("You need to provide key")
+        exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, REMOVE_GLOBAL_EVENT_PROPERTY, [params["key"]]);
     }
 };
 
-exports.removeAllGlobalEventProperties = function (success, error) {
-    exec(success, error, SMARTLOOK_PLUGIN, REMOVE_ALL_GLOBAL_EVENT_PROPERTIES, []);
+exports.removeAllGlobalEventProperties = function () {
+    exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, REMOVE_ALL_GLOBAL_EVENT_PROPERTIES, []);
 };
