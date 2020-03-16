7e431fff68620c9bcec8200b6c49c60a42d58d95
frantisespurny <frantisek.spurny1@gmail.com>
2020-03-16 17:35:37 +0100

    added missing api methods

--- src/android/com/smartlook/cordovaplugin/SmartlookPlugin.java
+++ src/android/com/smartlook/cordovaplugin/SmartlookPlugin.java
@@ -17,21 +17,39 @@ public class SmartlookPlugin extends CordovaPlugin {
     private static final String TAG = SmartlookPlugin.class.getSimpleName();
 
     // API methods names
+
+    // Setup and lifecycle
     private static final String SETUP_AND_START_RECORDING = "setupAndStartRecording";
     private static final String SETUP = "setup";
     private static final String START_RECORDING = "startRecording";
     private static final String STOP_RECORDING = "stopRecording";
     private static final String IS_RECORING = "isRecording";
+
+    // Fullscreen sensitive mode
     private static final String START_FULLSCREEN_SENSITIVE_MODE = "startFullscreenSensitiveMode";
     private static final String STOP_FULLSCREEN_SENSITIVE_MODE = "stopFullscreenSensitiveMode";
     private static final String IS_FULLSCREEN_SENSITIVE_MODE_ACTIVE = "isFullscreenSensitiveModeActive";
+
+    // User identification
     private static final String SET_USER_IDENTIFIER = "setUserIdentifier";
+
+    // Tracking
+    private static final String TRACK_NAVIGATION_EVENT = "trackNavigationEvent";
     private static final String START_TIMED_CUSTOM_EVENT = "startTimedCustomEvent";
+    private static final String STOP_TIMED_CUSTOM_EVENT = "stopTimedCustomEvent";
+    private static final String CANCEL_TIMED_CUSTOM_EVENT = "cancelTimedCustomEvent";
     private static final String TRACK_CUSTOM_EVENT = "trackCustomEvent";
+
+    // Event properties
     private static final String SET_GLOBAL_EVENT_PROPERTIES = "setGlobalEventProperties";
     private static final String SET_GLOBAL_EVENT_PROPERTY = "setGlobalEventProperty";
     private static final String REMOVE_GLOBAL_EVENT_PROPERTY = "removeGlobalEventProperty";
     private static final String REMOVE_ALL_GLOBAL_EVENT_PROPERTIES = "removeAllGlobalEventProperties";
+
+    // Utilities
+    private static final String SET_REFERRER = "setReferrer";
+    private static final String GET_DASHBOARD_SESSION_URL = "getDashboardSessionUrl";
+
 
     // Arguments
     private static final int SMARTLOOK_API_KEY = 0;
@@ -45,12 +63,17 @@ public class SmartlookPlugin extends CordovaPlugin {
     private static final int KEY = 0;
     private static final int VALUE = 1;
     private static final int IMMUTABLE_PROPERTY = 2;
+    private static final int VIEW_STATE = 1;
+    private static final int CUSTOM_EVENT_KEY = 1;
+    private static final int CUSTOM_EVENT_VALUE = 2;
+    private static final int EVENT_ID = 0;
+    private static final int REASON = 1;
+    private static final int CANCEL_TIMED_EVENT_PROPERTIES = 2;
+    private static final int REFERRER = 0;
+    private static final int SOURCE = 1;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
-        Log.d("xxxxx", "execute action: " + action + " args: " + args);
-
         if (action.equals(SETUP_AND_START_RECORDING)) {
             setupAndStartRecording(args, callbackContext);
         } else if (action.equals(SETUP)) {
@@ -69,8 +92,14 @@ public class SmartlookPlugin extends CordovaPlugin {
             isFullscreenSensitiveModeActive(callbackContext);
         } else if (action.equals(SET_USER_IDENTIFIER)) {
             setUserIdentifier(args, callbackContext);
+        } else if (action.equals(TRACK_NAVIGATION_EVENT)) {
+            trackNavigationEvent(args, callbackContext);
         } else if (action.equals(START_TIMED_CUSTOM_EVENT)) {
             startTimedCustomEvent(args, callbackContext);
+        } else if (action.equals(STOP_TIMED_CUSTOM_EVENT)) {
+            stopTimedCustomEvent(args, callbackContext);
+        } else if (action.equals(CANCEL_TIMED_CUSTOM_EVENT)) {
+            cancelTimedCustomEvent(args, callbackContext);
         } else if (action.equals(TRACK_CUSTOM_EVENT)) {
             trackCustomEvent(args, callbackContext);
         } else if (action.equals(SET_GLOBAL_EVENT_PROPERTIES)) {
@@ -81,6 +110,10 @@ public class SmartlookPlugin extends CordovaPlugin {
             removeGlobalEventProperty(args, callbackContext);
         } else if (action.equals(REMOVE_ALL_GLOBAL_EVENT_PROPERTIES)) {
             removeAllGlobalEventProperties(callbackContext);
+        } else if (action.equals(SET_REFERRER)) {
+            setReferrer(args, callbackContext);
+        } else if (action.equals(GET_DASHBOARD_SESSION_URL)) {
+            getDashboardSessionUrl(callbackContext);
         } else {
             callbackContext.error("Unknow action");
             return false;
@@ -88,6 +121,8 @@ public class SmartlookPlugin extends CordovaPlugin {
 
         return true;
     }
+
+    // Setup and lifecycle
 
     private void setupAndStartRecording(final JSONArray args, final CallbackContext callbackContext) throws JSONException {
 
@@ -98,8 +133,6 @@ public class SmartlookPlugin extends CordovaPlugin {
             callbackContext.success();
             return;
         } else if (!args.isNull(SMARTLOOK_API_KEY)) {
-            Log.d("xxxxx", "setupAndStartRecording: " + cordova.getActivity());
-
             Smartlook.setup(args.getString(SMARTLOOK_API_KEY), cordova.getActivity());
             Smartlook.unregisterBlacklistedClass(WebView.class);
             Smartlook.startRecording();
@@ -141,6 +174,8 @@ public class SmartlookPlugin extends CordovaPlugin {
         callbackContext.success(Smartlook.isRecording() ? "true" : "false");
     }
 
+    // Fullscreen sensitive mode
+
     private void startFullscreenSensitiveMode(CallbackContext callbackContext) throws JSONException {
         Smartlook.startFullscreenSensitiveMode();
         callbackContext.success();
@@ -154,6 +189,8 @@ public class SmartlookPlugin extends CordovaPlugin {
     private void isFullscreenSensitiveModeActive(CallbackContext callbackContext) throws JSONException {
         callbackContext.success(Smartlook.isFullscreenSensitiveModeActive() ? "true" : "false");
     }
+
+    // User identification
 
     private void setUserIdentifier(JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (!args.isNull(IDENTIFIER) && !args.isNull(SESSION_PROPERTIES)) {
@@ -169,23 +206,70 @@ public class SmartlookPlugin extends CordovaPlugin {
         callbackContext.error("Invalid setUserIdentifier parameters!");
     }
 
-    private void startTimedCustomEvent(JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (!args.isNull(EVENT_NAME) && !args.isNull(EVENT_PROPERTIES)) {
-            Smartlook.startTimedCustomEvent(args.getString(EVENT_NAME), args.getJSONObject(EVENT_PROPERTIES));
+    // Tracking
+
+    private void trackNavigationEvent(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (!args.isNull(EVENT_NAME) && !args.isNull(VIEW_STATE)) {
+            Smartlook.trackNavigationEvent(args.getString(EVENT_NAME), args.getString(VIEW_STATE));
             callbackContext.success();
             return;
+        }
+
+        callbackContext.error("Invalid trackNavigationEvent parameters!");
+    }
+
+    private void startTimedCustomEvent(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (!args.isNull(EVENT_NAME) && !args.isNull(EVENT_PROPERTIES)) {
+            String eventId = Smartlook.startTimedCustomEvent(args.getString(EVENT_NAME), args.getJSONObject(EVENT_PROPERTIES));
+            callbackContext.success(eventId);
+            return;
         } else if (!args.isNull(EVENT_NAME)) {
-            Smartlook.startTimedCustomEvent(args.getString(EVENT_NAME));
-            callbackContext.success();
+            String eventId = Smartlook.startTimedCustomEvent(args.getString(EVENT_NAME));
+            callbackContext.success(eventId);
             return;
         }
 
         callbackContext.error("Invalid startTimedCustomEvent parameters!");
     }
 
+    private void stopTimedCustomEvent(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (!args.isNull(EVENT_ID) && !args.isNull(EVENT_PROPERTIES)) {
+            String eventId = Smartlook.stopTimedCustomEvent(args.getString(EVENT_NAME), args.getJSONObject(EVENT_PROPERTIES));
+            callbackContext.success();
+            return;
+        } else if (!args.isNull(EVENT_ID)) {
+            String eventId = Smartlook.stopTimedCustomEvent(args.getString(EVENT_NAME));
+            callbackContext.success();
+            return;
+        }
+
+        callbackContext.error("Invalid stopTimedCustomEvent parameters!");
+    }
+
+    private void cancelTimedCustomEvent(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (!args.isNull(EVENT_ID) && !args.isNull(REASON) && !args.isNull(CANCEL_TIMED_EVENT_PROPERTIES)) {
+            String eventId = Smartlook.cancelTimedCustomEvent(
+                args.getString(EVENT_NAME),
+                args.getString(REASON),
+                args.getJSONObject(CANCEL_TIMED_EVENT_PROPERTIES));
+            callbackContext.success();
+            return;
+        } else if (!args.isNull(EVENT_ID) && !args.isNull(REASON)) {
+            String eventId = Smartlook.cancelTimedCustomEvent(args.getString(EVENT_NAME), args.getString(REASON));
+            callbackContext.success();
+            return;
+        }
+
+        callbackContext.error("Invalid cancelTimedCustomEvent parameters!");
+    }
+
     private void trackCustomEvent(JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (!args.isNull(EVENT_NAME) && !args.isNull(EVENT_PROPERTIES)) {
             Smartlook.trackCustomEvent(args.getString(EVENT_NAME), args.getJSONObject(EVENT_PROPERTIES));
+            callbackContext.success();
+            return;
+        } else if (!args.isNull(EVENT_NAME) && !args.isNull(CUSTOM_EVENT_KEY) && !args.isNull(CUSTOM_EVENT_VALUE)) {
+            Smartlook.trackCustomEvent(args.getString(EVENT_NAME), args.getString(CUSTOM_EVENT_KEY), args.getString(CUSTOM_EVENT_VALUE));
             callbackContext.success();
             return;
         } else if (!args.isNull(EVENT_NAME)) {
@@ -196,6 +280,8 @@ public class SmartlookPlugin extends CordovaPlugin {
 
         callbackContext.error("Invalid trackCustomEvent parameters!");
     }
+
+    // Event properties
 
     private void setGlobalEventProperties(JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (!args.isNull(GLOBAL_EVENT_PROPERTIES) && !args.isNull(IMMUTABLE_PROPERTIES)) {
@@ -231,4 +317,21 @@ public class SmartlookPlugin extends CordovaPlugin {
         Smartlook.removeAllGlobalEventProperties();
         callbackContext.success();
     }
+
+    // Utilities
+
+    private void setReferrer(JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (!args.isNull(REFERRER) && !args.isNull(SOURCE)) {
+            Smartlook.setReferrer(args.getString(REFERRER), args.getString(SOURCE));
+            callbackContext.success();
+            return;
+        }
+
+        callbackContext.error("Invalid setReferrer parameters!");
+    }
+
+    private void getDashboardSessionUrl(CallbackContext callbackContext) throws JSONException {
+        callbackContext.success(Smartlook.getDashboardSessionUrl);
+    }
+
 }
--- www/SmartlookPlugin.js
+++ www/SmartlookPlugin.js
@@ -4,24 +4,47 @@ var exec = require('cordova/exec');
 const SMARTLOOK_PLUGIN = "SmartlookPlugin"
 
 // API methods names
+
+// Setup and lifecycle
 const SETUP_AND_START_RECORDING = "setupAndStartRecording";
 const SETUP = "setup";
 const START_RECORDING = "startRecording";
 const STOP_RECORDING = "stopRecording";
 const IS_RECORING = "isRecording";
+
+// Fullscreen sensitive mode
 const START_FULLSCREEN_SENSITIVE_MODE = "startFullscreenSensitiveMode";
 const STOP_FULLSCREEN_SENSITIVE_MODE = "stopFullscreenSensitiveMode";
 const IS_FULLSCREEN_SENSITIVE_MODE_ACTIVE = "isFullscreenSensitiveModeActive";
+
+// User identifier
 const SET_USER_IDENTIFIER = "setUserIdentifier";
+
+//Tracking
+const TRACK_NAVIGATION_EVENT = "trackNavigationEvent"
 const START_TIMED_CUSTOM_EVENT = "startTimedCustomEvent";
+const STOP_TIMED_CUSTOM_EVENT = "stopTimedCustomEvent";
+const CANCEL_TIMED_CUSTOM_EVENT = "cancelTimedCustomEvent";
 const TRACK_CUSTOM_EVENT = "trackCustomEvent";
+
+// Event properties
 const SET_GLOBAL_EVENT_PROPERTIES = "setGlobalEventProperties";
 const SET_GLOBAL_EVENT_PROPERTY = "setGlobalEventProperty";
 const REMOVE_GLOBAL_EVENT_PROPERTY = "removeGlobalEventProperty";
 const REMOVE_ALL_GLOBAL_EVENT_PROPERTIES = "removeAllGlobalEventProperties";
 
+// Utilities
+const SET_REFERRER = "setReferrer"
+const GET_DASHBOARD_SESSION_URL = "getDashboardSessionUrl";
+
+
 var emptyCallback = function() { return; };
 
+
+// Setup and lifecycle
+
+// setupAndStart(smartlookAPIKey)
+// setupAndStart(smartlookAPIKey, fps)
 exports.setupAndStartRecording = function (params) {
     if (params["smartlookAPIKey"] != undefined) {
         if (params["fps"] != undefined) {
@@ -32,6 +55,8 @@ exports.setupAndStartRecording = function (params) {
     }
 };
 
+// setup(smartlookAPIKey)
+// setup(smartlookAPIKey, fps)
 exports.setup = function (params) {
     if (params["smartlookAPIKey"] != undefined) {
         if (params["fps"] != undefined) {
@@ -42,30 +67,49 @@ exports.setup = function (params) {
     }
 };
 
+// startRecording()
 exports.startRecording = function () {
     exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_RECORDING, []);
 };
 
+// stopRecording()
 exports.stopRecording = function () {
     exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, STOP_RECORDING, []);
 };
 
+// isRecording()
 exports.isRecording = function (result) {
     exec(result, emptyCallback, SMARTLOOK_PLUGIN, IS_RECORING, []);
 };
 
-exports.startFullscreenSensitiveMode = function () {
-    exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_FULLSCREEN_SENSITIVE_MODE, []);
+
+// Fullscreen sensitive mode
+
+// startFullScreenSenstiveMode()
+// startFullScreenSenstiveMode(color)
+exports.startFullscreenSensitiveMode = function (params) {
+    if (params["color"] != undefined) {
+        exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_FULLSCREEN_SENSITIVE_MODE, [params["color"]]);
+    } else {
+        exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_FULLSCREEN_SENSITIVE_MODE, []);
+    }
 };
 
+// stopFullScreenSenstiveMode()
 exports.stopFullscreenSensitiveMode = function () {
     exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, STOP_FULLSCREEN_SENSITIVE_MODE, []);
 };
 
+// isFullscreenModeActive(result)
 exports.isFullscreenSensitiveModeActive = function (result) {
     exec(result, emptyCallback, SMARTLOOK_PLUGIN, IS_FULLSCREEN_SENSITIVE_MODE_ACTIVE, []);
 };
 
+
+// User identification
+
+// setUserIdentifier(identifier)
+// setUserIdentifier(identifier, sessionProperties)
 exports.setUserIdentifier = function (params) {
     if (params["identifier"] != undefined) {
         if (params["sessionProperties"] != undefined) {
@@ -76,44 +120,106 @@ exports.setUserIdentifier = function (params) {
     }
 };
 
-exports.startTimedCustomEvent = function (params) {
+
+// Tracking
+
+// todo viewstate constant 
+// trackNavigationEvent(name, viewState)
+exports.trackNavigationEvent = function (params) {
+    if (params["name"] != undefined && params["viewState"] != undefined) {
+        exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, TRACK_NAVIGATION_EVENT, [params["name"], params["viewState"]]);
+    }
+};
+
+// startTimedCustomEvent(name): String
+// startTimedCustomEvent(name, eventProperties): String
+exports.startTimedCustomEvent = function (result, params) {
     if (params["name"] != undefined) {
         if (params["eventProperties"] != undefined) {
-            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_TIMED_CUSTOM_EVENT, [params["name"], params["eventProperties"]]);
+            exec(result, emptyCallback, SMARTLOOK_PLUGIN, START_TIMED_CUSTOM_EVENT, [params["name"], params["eventProperties"]]);
         } else {
-            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, START_TIMED_CUSTOM_EVENT, [params["name"]]);
+            exec(result, emptyCallback, SMARTLOOK_PLUGIN, START_TIMED_CUSTOM_EVENT, [params["name"]]);
         }
     }
 };
 
+// stopTimedCustomEvent(eventId)
+// stopTimedCustomEvent(eventId, eventProperties)
+exports.stopTimedCustomEvent = function (result, params) {
+    if (params["eventId"] != undefined) {
+        if (params["eventProperties"] != undefined) {
+            exec(result, emptyCallback, SMARTLOOK_PLUGIN, STOP_TIMED_CUSTOM_EVENT, [params["eventId"], params["eventProperties"]]);
+        } else {
+            exec(result, emptyCallback, SMARTLOOK_PLUGIN, STOP_TIMED_CUSTOM_EVENT, [params["eventId"]]);
+        }
+    }
+};
+
+// cancelTimedCustomEvent(eventId, reason)
+// cancelTimedCustomEvent(eventId, reason, eventProperties)
+exports.cancelTimedCustomEvent = function (result, params) {
+    if (params["name"] != undefined && params["reason"]) {
+        if (params["eventProperties"] != undefined) {
+            exec(result, emptyCallback, SMARTLOOK_PLUGIN, CANCEL_TIMED_CUSTOM_EVENT, [params["name"], params["reason"], params["eventProperties"]]);
+        } else {
+            exec(result, emptyCallback, SMARTLOOK_PLUGIN, CANCEL_TIMED_CUSTOM_EVENT, [params["name"], params["reason"]]);
+        }
+    }
+};
+
+// trackCustomEvent(name)
+// trackCustomEvent(name, eventProperties)
+// trackCustomEvent(name, key, value)
 exports.trackCustomEvent = function (params) {
     if (params["name"] != undefined) {
         if (params["eventProperties"] != undefined) {
             exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, TRACK_CUSTOM_EVENT, [params["name"], params["eventProperties"]]);
+        } else if (params["key"] != undefined && params["value"] != undefined) {
+            exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, TRACK_CUSTOM_EVENT, [params["name"], params["key"], params["value"]]);
         } else {
             exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, TRACK_CUSTOM_EVENT, [params["name"]]);
         }
     }
 };
 
+// Event properties
+
+// setGlobalEventProperties(globalEventProperties, immutable)
 exports.setGlobalEventProperties = function (params) {
     if (params["globalEventProperties"] != undefined && params["immutable"] != undefined) {
         exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SET_GLOBAL_EVENT_PROPERTIES, [params["globalEventProperties"], params["immutable"]]);
     }
 };
 
+// setGlobalEventProperty(key, value, immutable)
 exports.setGlobalEventProperty = function (params) {
     if (params["key"] != undefined && params["value"] != undefined && params["immutable"] != undefined) {
         exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SET_GLOBAL_EVENT_PROPERTY, [params["key"], params["value"], params["immutable"]]);
     }
 };
 
+// removeGlobalEventProperty(key)
 exports.removeGlobalEventProperty = function (params) {
     if (params["key"] != undefined) {
         exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, REMOVE_GLOBAL_EVENT_PROPERTY, [params["key"]]);
     }
 };
 
+// removeAllGlobalEventProperties()
 exports.removeAllGlobalEventProperties = function () {
     exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, REMOVE_ALL_GLOBAL_EVENT_PROPERTIES, []);
+};
+
+// Utilities
+
+// setReferrer(referrer, source)
+exports.setReferrer = function (params) {
+    if (params["referrer"] != undefined && params["source"] != undefined) {
+        exec(emptyCallback, emptyCallback, SMARTLOOK_PLUGIN, SET_REFERRER, [params["referrer"], params["source"]]);
+    }
+}
+
+// getDashboardSessionUrl(): String
+exports.getDashboardSessionUrl = function (result) {
+    exec(result, emptyCallback, SMARTLOOK_PLUGIN, GET_DASHBOARD_SESSION_URL, []);
 };
