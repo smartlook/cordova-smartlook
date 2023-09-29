efec8242dfc7d9ea085587e71c2306af4a80867a
c0mtru1se <martinbudinsky1@gmail.com>
2023-09-29 13:29:48 +0200

    chore(release): release 2.0.3

--- src/js/cordova-plugin-smartlook.ts
+++ src/js/cordova-plugin-smartlook.ts
@@ -3,7 +3,7 @@ const SMARTLOOK_PLUGIN = 'SmartlookPlugin';
 
 // Smartlook framework info
 export const SMARTLOOK_FRAMEWORK_VERSION = '-';
-export const SMARTLOOK_FRAMEWORK_PLUGIN_VERSION = '2.0.2';
+export const SMARTLOOK_FRAMEWORK_PLUGIN_VERSION = '2.0.3';
 
 // API methods names
 export enum Command {
