c53f3523376b94c8a7b80899bb6ebac183e1cb9c
Martin Budínský <budim@cisco.com>
2024-04-24 15:09:51 +0200

    chore(lib): release 2.0.4

--- www/cordova-plugin-smartlook.js
+++ www/cordova-plugin-smartlook.js
@@ -6,7 +6,7 @@ exports.enableLogs = exports.setRecordingMask = exports.removeRecordingStatusCha
 var SMARTLOOK_PLUGIN = 'SmartlookPlugin';
 // Smartlook framework info
 exports.SMARTLOOK_FRAMEWORK_VERSION = '-';
-exports.SMARTLOOK_FRAMEWORK_PLUGIN_VERSION = '2.0.3';
+exports.SMARTLOOK_FRAMEWORK_PLUGIN_VERSION = '2.0.4';
 // API methods names
 var Command;
 (function (Command) {
